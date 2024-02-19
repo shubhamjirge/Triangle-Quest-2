ffc4480361d4b43b64ec1e522d1d82a8281ccba6
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2024-02-19 10:57:56 +0530

    Add files via upload

---
+++ TriangleQuest2.py
@@ -0,0 +1,9 @@
+"""
+Title     : Triangle Quest 2
+Subdomain : Math
+Domain    : Python
+"""
+for i in range(
+    1, int(input()) + 1
+):  # More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(((10**i - 1) // 9) ** 2)
